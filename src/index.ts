3dca8c9f7a242abdb5ff2c11bb0bb6b2b0642331
benjin <benjin@umich.edu>
2023-02-16 22:24:49 -0500

    Timer and idle/busy

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
+  ILabStatus
 } from '@jupyterlab/application';
+
+import { IStatusBar } from '@jupyterlab/statusbar';
+
+import { Widget } from '@lumino/widgets';
+
 
 /**
  * Initialization data for the jupyterlab_widget extension.
@@ -9,8 +15,41 @@ import {
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_widget:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
+  requires: [ILabStatus, IStatusBar],
+  activate: (app: JupyterFrontEnd, labStatus: ILabStatus, statusBar: IStatusBar) => {
     console.log('JupyterLab extension jupyterlab_widget is activated!');
+    let startTime = Date.now();
+    const statusWidget = new Widget();
+    const timeWidget = new Widget();
+
+    let statusWidgetID = statusWidget
+
+
+    labStatus.busySignal.connect(() => {
+      statusWidget.node.textContent = labStatus.isBusy ? 'Ben Busy' : 'Ben Idle';
+      if (statusWidget.node.textContent === "Ben Busy") {
+        startTime = Date.now();
+        console.log("Cell Ran");
+        // setInterval()
+      }
+      if (statusWidget.node.textContent === "Ben Idle") {
+        console.log("stopped")
+        timeWidget.node.textContent = (Date.now() - startTime).toString().concat(" ms");
+      }
+    });
+
+    statusBar
+
+    statusBar.registerStatusItem('lab-status', {
+      align: 'left',
+      item: statusWidget
+    });
+
+    statusBar.registerStatusItem('time-status', {
+      align: 'middle',
+      item: timeWidget
+    });
+
   }
 };
 
